fd33867eabffc1439b8df5b111709df0fb69a26e
adesso <adesso@DE-ADN-JGMXFH2.adesso.local>
2020-07-11 15:23:45 +0200

    takeUntil

--- src/app/character/character/character.component.ts
+++ src/app/character/character/character.component.ts
@@ -1,20 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Character} from "../model/character";
 import {ActivatedRoute} from "@angular/router";
-import {map, pluck, switchMap} from "rxjs/operators";
+import {map, pluck, switchMap, takeUntil} from "rxjs/operators";
 import {CharacterService} from "../character.service";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'ngb-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
 
   characters: Character[] = [
     {id: 1, name: 'Daenerys Targaryen', culture: 'Valyrian'},
     {id: 2, name: 'Jon Snow', culture: 'Northmen'}
   ];
+
+  destroy: Subject<boolean> = new Subject<boolean>();
 
   character: Character;
 
@@ -22,9 +25,15 @@ export class CharacterComponent implements OnInit {
     this.route.params.pipe(
       pluck('id'),
       map((param: string) => Number(param)),
-      switchMap((id: number) => this.characterService.getById(id))
+      switchMap((id: number) => this.characterService.getById(id)),
+      takeUntil(this.destroy)
     )
       .subscribe((character: Character) => this.character = character);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.complete();
   }
 
   ngOnInit(): void {
